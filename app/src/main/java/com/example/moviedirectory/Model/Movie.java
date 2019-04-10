9fa0e40bf6a141f1f373d12c5640585a9e3ad5a1
RIYA3004 <riya.suri@somaiya.edu>
2019-04-10 21:56:32 +0530

    creation of Movie.java in Model Package

---
+++ app/src/main/java/com/example/moviedirectory/Model/Movie.java
@@ -0,0 +1,161 @@
+package com.example.moviedirectory.Model;
+import java.io.Serializable;
+public class Movie implements Serializable {
+    private static final long id=1L;
+
+
+
+    private String title;
+    private String director;
+    private String poster;
+    private String genre;
+    private String writer;
+    private String actors;
+    private String plot;
+    private String rating;
+    private String dvdRelease;
+    private String country;
+    private String awards;
+    private String tvRated;
+    private String movieType;
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    private String runTime;
+    private String imdbId;
+
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDvdRelease() {
+        return dvdRelease;
+    }
+
+    public void setDvdRelease(String dvdRelease) {
+        this.dvdRelease = dvdRelease;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAwards() {
+        return awards;
+    }
+
+    public void setAwards(String awards) {
+        this.awards = awards;
+    }
+
+    public String getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(String movieType) {
+        this.movieType = movieType;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getTvRated() {
+        return tvRated;
+    }
+
+    public void setTvRated(String tvRated) {
+        this.tvRated = tvRated;
+    }
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    private String year;
+
+    public String getRunTime() {
+        return runTime;
+    }
+
+    public void setRunTime(String runTime) {
+        this.runTime = runTime;
+    }
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+
+
+    public Movie()
+    {
+
+    }
+
+}
