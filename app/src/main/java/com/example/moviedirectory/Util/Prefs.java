4f183b0049d96abd6bfd75e34fc5a09895f20c1d
RIYA3004 <riya.suri@somaiya.edu>
2019-04-17 21:21:13 +0530

    Updation of the content in Prefs

--- app/src/main/java/com/example/moviedirectory/Util/Prefs.java
+++ app/src/main/java/com/example/moviedirectory/Util/Prefs.java
@@ -1,4 +1,17 @@
 package com.example.moviedirectory.Util;
 
+import android.content.SharedPreferences;
+import android.app.Activity;
 public class Prefs {
+    SharedPreferences sharedPreferences;
+    public Prefs(Activity activity){
+        sharedPreferences =activity.getPreferences(activity.MODE_PRIVATE);
+    }
+    public void setSearch(String search)
+    {
+        sharedPreferences.edit().putString("search",search).commit();//saves the user preferences
+    }
+    public String getSearch(){
+        return sharedPreferences.getString("search","SuperMan");
+    }
 }
