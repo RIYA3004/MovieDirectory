58dfe0dc1a32ae2416667bcf097ce11596c0a1ba
RIYA3004 <riya.suri@somaiya.edu>
2019-04-22 15:39:19 +0530

    Making of the cardview to show the movie details and the Adapter Creation

--- app/src/main/java/com/example/moviedirectory/Data/MovieRecyclerViewAdapter.java
+++ app/src/main/java/com/example/moviedirectory/Data/MovieRecyclerViewAdapter.java
@@ -1,4 +1,42 @@
 package com.example.moviedirectory.Data;
 
-public class MovieRecyclerViewAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.moviedirectory.Model.Movie;
+
+import java.util.List;
+
+public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecyclerViewAdapter.ViewHolder> {
+    private Context context;
+    private List<Movie> movieList;
+    public MovieRecyclerViewAdapter(Context context, List<Movie> movies) {
+        this.context=context;
+        movieList=movies;
+    }
+
+    @NonNull
+    @Override
+    public MovieRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieRecyclerViewAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
