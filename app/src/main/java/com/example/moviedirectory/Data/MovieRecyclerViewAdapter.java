aea43b44ba02a57240cf6cb8bb62f030b09f19b8
RIYA3004 <riya.suri@somaiya.edu>
2019-04-22 17:08:16 +0530

    More updation in volley and inclusion of picasso

--- app/src/main/java/com/example/moviedirectory/Data/MovieRecyclerViewAdapter.java
+++ app/src/main/java/com/example/moviedirectory/Data/MovieRecyclerViewAdapter.java
@@ -3,10 +3,16 @@ package com.example.moviedirectory.Data;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.moviedirectory.Model.Movie;
+import com.example.moviedirectory.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -21,12 +27,22 @@ public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecycler
     @NonNull
     @Override
     public MovieRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_row, parent, false);
+        return new ViewHolder(view,context);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MovieRecyclerViewAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+      Movie movie=movieList.get(position);
+      String posterLink=movie.getPoster();
+      holder.title.setText(movie.getTitle());
+      holder.type.setText(movie.getMovieType());
+      //picasso library
+        Picasso.get()
+                .load(posterLink)
+                 .placeholder(android.R.drawable.ic_btn_speak_now)
+                 .into(holder.poster);
+        holder.year.setText(movie.getYear());
     }
 
     @Override
@@ -34,9 +50,30 @@ public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecycler
         return movieList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
-        public ViewHolder(View itemView) {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView title;
+        ImageView poster;
+        TextView year;
+        TextView type;
+        public ViewHolder(View itemView,Context ctx) {
             super(itemView);
+            context=ctx;
+            title=(TextView) itemView.findViewById(R.id.movieTitleID);
+            poster=(ImageView) itemView.findViewById(R.id.movieImageID);
+            year=(TextView) itemView.findViewById(R.id.movieReleaseID);
+            type=(TextView) itemView.findViewById(R.id.movieCatID);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                 Toast.makeText(context,"Row Tapped!",Toast.LENGTH_LONG).show();
+
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View v) {
+
         }
     }
 }
